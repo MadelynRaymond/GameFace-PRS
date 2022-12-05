485405e6ed0ca005a84efe9fbc402c9d5ce1648b
Matt <n01437978@unf.edu>
2022-12-05 01:06:13 -0500

    added login page layout

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,7 +1,20 @@
+import { Link } from "@remix-run/react";
+
 export default function Login() {
     return (
-        <div>
-            
+        <div style={{height: 'calc(100vh - 160px'}} className="login-container">
+            <form id="login" className='box'>
+                <h1>Login</h1>
+                <label>Username</label>
+                <input type="text" name="name" id="name" placeholder="Username"/>
+                <label>Password</label>
+                <input type="Password" name="pass" id="pass" placeholder="Password"/>
+                <input type="button" name="login"value="Login"/>
+                
+                <span>Remember me</span>
+                <input style={{display: 'inline'}} type="checkbox" id="check"/>
+                <Link to="/forgot-password">Forgot password?</Link>
+            </form>
         </div>
     )
 }
