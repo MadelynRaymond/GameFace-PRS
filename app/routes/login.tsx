b2bd70f19f6a6bd308ef8e4ddf43461f012172aa
Matt <n01437978@unf.edu>
2022-12-05 01:57:47 -0500

    added login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,20 +1,73 @@
-import { Link } from "@remix-run/react";
+import type { ActionArgs, LoaderArgs } from "@remix-run/node";
+import {redirect} from '@remix-run/node'
+import { json } from "@remix-run/node";
+import { Form, Link, useActionData } from "@remix-run/react";
+import React from "react";
+import { verifyLogin } from "~/models/user.server";
+import { createUserSession, getUserId } from "~/session.server";
+
+
+
+export async function loader({request}: LoaderArgs) {
+    const userId = await getUserId(request)
+    if (userId) return redirect('/user/stats/overall')
+    return null
+}
+
+
+export async function action({request}: ActionArgs) {
+    const formData = await request.formData()
+    const email = formData.get("email")
+    const password = formData.get("password")
+
+    console.log(email)
+  
+    if (typeof email !== "string" || email.length === 0){
+      return json({errors: {email: "Email is required", password: null}}, {status: 400})
+    } 
+  
+    if(typeof password !== "string" || password.length === 0) {
+      return json({errors: {email: null, password: "Password is required"}}, {status: 400})
+    }
+  
+    const user = await verifyLogin(email, password)
+  
+    if (!user) {
+      return json({errors: {email: null, password: "Invalid username or password"}},  {status: 400})
+    }
+  
+    return createUserSession({
+      request,
+      userId: user.id,
+      remember: true,
+      redirectTo: `/${user.email}/stats`
+    })
+  
+  }
 
 export default function Login() {
+    const actionData = useActionData<typeof action>()
+    const email = React.useRef<HTMLInputElement>(null)
+    const password = React.useRef<HTMLInputElement>(null)
+
     return (
-        <div style={{height: 'calc(100vh - 160px'}} className="login-container">
-            <form id="login" className='box'>
+        <div style={{height: '85vh'}} className="login-container">
+            <Form method="post" id="login">
                 <h1>Login</h1>
-                <label>Username</label>
-                <input type="text" name="name" id="name" placeholder="Username"/>
-                <label>Password</label>
-                <input type="Password" name="pass" id="pass" placeholder="Password"/>
-                <input type="button" name="login"value="Login"/>
+                <div>
+                    <input ref={email} type="text" name="email" id="email" placeholder="Email"/>
+                    <span className="error-text">{actionData?.errors?.email}</span>
+                </div>
+                <div>
+                    <input ref={password} type="Password" name="password" id="password" placeholder="Password"/>
+                    <span className="error-text">{actionData?.errors?.password}</span>
+                </div>
+                <input type="submit" name="login"value="Login"/>
                 
                 <span>Remember me</span>
                 <input style={{display: 'inline'}} type="checkbox" id="check"/>
                 <Link to="/forgot-password">Forgot password?</Link>
-            </form>
+            </Form>
         </div>
     )
 }
