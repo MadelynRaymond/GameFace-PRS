625dd9c1583d69d5d370bce98e280866e6cef053
Matt <n01437978@unf.edu>
2022-12-05 00:25:38 -0500

    added shooting category layout

--- app/routes/$username/stats.tsx
+++ app/routes/$username/stats.tsx
@@ -1,14 +1,16 @@
-import { NavLink } from "@remix-run/react";
+import { NavLink, Outlet } from "@remix-run/react";
 
 export default function Stats() {
   return (
-    <div className="stats-menu">
-      <p className="stats-menu__header">Performance Stats</p>
-      <p className="stats-menu__athlete-name">Athlete: Danielle Robbins</p>
-      <div className="stats-menu__items">
-        {["overall", "speed", "shooting", "dribbling", "passing", "strength", "jumping"].map((category, i) => <NavLink className={({isActive}) => isActive ? 'stats-menu__item-selected' : undefined} key={i} to={category}>{category}</NavLink>)}
+    <>
+      <div className="stats-menu">
+        <p className="stats-menu__header">Performance Stats</p>
+        <p className="stats-menu__athlete-name">Athlete: Danielle Robbins</p>
+        <div className="stats-menu__items">
+          {["overall", "speed", "shooting", "dribbling", "passing", "strength", "jumping"].map((category, i) => <NavLink className={({isActive}) => isActive ? 'stats-menu__item-selected' : undefined} key={i} to={category}>{category}</NavLink>)}
+        </div>
       </div>
-
-    </div>
+      <Outlet/>
+    </>
   )
 }
