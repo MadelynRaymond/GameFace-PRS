a756eec385469a7985dfcff1dde2d7612412bcbc
Matt <n01437978@unf.edu>
2022-12-06 11:27:23 -0500

    logouts user when no session

--- app/session.server.ts
+++ app/session.server.ts
@@ -87,7 +87,7 @@ export async function createUserSession({
 
 export async function logout(request: Request) {
   const session = await getSession(request);
-  return redirect("/", {
+  return redirect("/login", {
     headers: {
       "Set-Cookie": await sessionStorage.destroySession(session),
     },
