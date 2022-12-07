025a55cd08a4af0614c8bd7ad9d2c848be11c5d5
Matt <n01437978@unf.edu>
2022-12-07 12:09:08 -0500

    fetch categories in parallel

--- app/routes/staff/athletes/$athleteId.tsx
+++ app/routes/staff/athletes/$athleteId.tsx
@@ -3,29 +3,39 @@ import { json, Response } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
 import { getAthleteWithReports } from "~/models/athlete.server";
+import { getExerciseCategories } from "~/models/exercise-category.server";
 
 export async function loader({request, params}: LoaderArgs) {
   invariant(params.athleteId, "Not athlete id in params")
 
   const athleteId = parseInt(params.athleteId)
-  const athlete = await getAthleteWithReports(athleteId)
+
+
+  const [athlete, categories] = await Promise.all([getAthleteWithReports(athleteId), getExerciseCategories()])
 
   if (!athlete) {
     throw new Response("Not Found", {status: 404})
   }
 
-  return json({reports: athlete.reports, profile: athlete.profile})
+  if(!categories) {
+    throw new Response("Unexpected Error", {status: 500})
+  }
+
+  return json({reports: athlete.reports, profile: athlete.profile, categories})
 
 }
 
 export default function AthleteDetails() {
 
-  const {reports, profile} = useLoaderData<typeof loader>()
+  const {reports, profile, categories} = useLoaderData<typeof loader>()
 
   return (
     <div className="athlete-overview-container">
       <div className="athlete-reports">
-        
+        <h2>{profile?.firstName} {profile?.lastName}</h2>
+        <pre>{JSON.stringify(reports)}</pre>
+        <pre>{JSON.stringify(profile)}</pre>
+        <pre>{JSON.stringify(categories)}</pre>
       </div>
       <div className="athlete-report-form">
         <label htmlFor="time-drill">Completion time: </label>
