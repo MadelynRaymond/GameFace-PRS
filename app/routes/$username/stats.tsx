9d481e6ebb06c9aaa03923c3fd11d16912fd7122
Matt <n01437978@unf.edu>
2022-12-09 11:16:59 -0500

    shows unauthorized on navigate to other user stats

--- app/routes/$username/stats.tsx
+++ app/routes/$username/stats.tsx
@@ -1,18 +1,22 @@
 import type { LoaderArgs } from '@remix-run/node'
 import { json, Response } from '@remix-run/node'
 import { NavLink, Outlet, useCatch, useLoaderData, useLocation } from '@remix-run/react'
+import invariant from 'tiny-invariant'
 import { getAthleteById } from '~/models/athlete.server'
 import { requireUserId } from '~/session.server'
 
-export async function loader({ request }: LoaderArgs) {
+export async function loader({ request, params }: LoaderArgs) {
     const userId = await requireUserId(request)
-
+    invariant(params.username, "username not found")
     const athlete = await getAthleteById(userId)
 
     if (!athlete) {
         throw new Response('Not Found', { status: 404 })
     }
 
+    if(params.username !== athlete.username) {
+        throw new Response("Not Authorized", {status: 400})
+    }
     return json({
         profile: athlete.profile,
         email: athlete.email,
