53349b80d109443bee2b28dbb05d1d3225e586a3
Matt <n01437978@unf.edu>
2023-02-06 20:37:21 -0500

    added cool button to login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -89,7 +89,7 @@ export default function Login() {
                     <span className="error-text">{actionData?.errors?.password}</span>
                 </div>
                 <div className="login-btn-group">
-                    <input className="login-button" type="submit" name="login" value="Login" />
+                    <input className="btn" type="submit" name="login" value="Login" />
                     <Link className="forgot-password-link" to="/forgot-password">
                         Forgot password?
                     </Link>
