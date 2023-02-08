f6855b0ab8c9498a9c5dd6a67a6bcaee73f52de7
Bryan Torres <109679220+torbryan@users.noreply.github.com>
2023-02-08 01:12:37 -0500

    Reset password (#28)
    * Added Zod ChangePasswordSchema
    
    * Fixed Redundent Code. WIP loading route
    
    * Fixed loader, action, and error for cp
    
    * Added Comments
    
    Added Comments on Loader and Action purpose
    
    * ChangePassword file name switched
    
    * Fixed action,loader, and fixed name/id
    
    * Fixed rest-link
    
    * changed name of file
    
    * Fixes #28 (#29)
    
    ---------
    
    Co-authored-by: mreid21 <63518180+mreid21@users.noreply.github.com>

---
+++ app/routes/$username/change-password.tsx
@@ -0,0 +1,67 @@
+import { ActionArgs, LoaderArgs, json, redirect } from '@remix-run/node'
+import { Form, useActionData, useLoaderData, useTransition } from '@remix-run/react'
+import { ZodError, z } from 'zod'
+import { changePassword } from '~/models/user.server'
+import { requireUser, requireUserId } from '~/session.server'
+
+export const ChangePasswordSchema = z
+    .object({
+        newPassword: z.string().min(4),
+        confirmPassword: z.string().min(4),
+    })
+    .refine((data) => data.newPassword === data.confirmPassword, 'Passwords must match')
+
+type ChangePasswordErrors = z.inferFlattenedErrors<typeof ChangePasswordSchema>
+type ActionData = {
+    errors?: ChangePasswordErrors
+    updateError?: { message: string }
+}
+// Loader will get called when the page loads
+export async function loader({ request }: LoaderArgs) {
+    const userId = await requireUserId(request)
+    if (!userId) return redirect('/change-password/password')
+    return json({ userId })
+}
+// Action will get called when you submit the form
+export async function action({ request }: ActionArgs) {
+    const { username, id } = await requireUser(request)
+    try {
+        const formData = await request.formData()
+        const data = await Object.fromEntries(formData)
+        const validatedData = await ChangePasswordSchema.parseAsync(data)
+
+        await changePassword({ userId: id, password: validatedData.newPassword })
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return json({ errors: error.flatten() as ChangePasswordErrors })
+        }
+    }
+}
+
+export default function ChangePassword() {
+    const actionData = useActionData<ActionData>()
+    const transition = useTransition()
+    const { userId } = useLoaderData<typeof loader>()
+
+    return (
+        <div
+            style={{
+                height: '75vh',
+            }}
+            className="form-center"
+        >
+            <Form method="post" style={{maxWidth: '600px', padding: '1rem'}}>
+                <h1>Change Password</h1>
+                <input type="password" name="newPassword" id="newPassword" placeholder="New Password" />
+                {actionData?.errors?.fieldErrors.newPassword && <span className="error-text">{actionData.errors.fieldErrors.newPassword[0]}</span>}
+                <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password" />
+                {actionData?.errors?.fieldErrors.confirmPassword && <span className="error-text">{actionData.errors.fieldErrors.confirmPassword[0]}</span>}
+                <input type="hidden" name="user-id" value={userId} />
+                <div>
+                    <input className="btn" disabled={transition.state === 'loading'} type="submit" value="Change password" />
+                    {actionData?.errors?.formErrors && <span className="error-text">{actionData.errors.formErrors[0]}</span>}
+                </div>
+            </Form>
+        </div>
+    )
+}
