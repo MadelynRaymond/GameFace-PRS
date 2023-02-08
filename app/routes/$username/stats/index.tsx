261a8b456319696830e56466c6bce966f6f3fbe5
mreid21 <63518180+mreid21@users.noreply.github.com>
2023-02-08 03:18:21 -0500

    added download csv (#31)

--- app/routes/$username/stats/index.tsx
+++ app/routes/$username/stats/index.tsx
@@ -1,12 +1,10 @@
 import type { LoaderArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import { useFetcher, useLoaderData } from '@remix-run/react'
+import { useEffect, useState } from 'react'
+import { CSVLink } from 'react-csv'
 import { CartesianGrid, Legend, Line, LineChart, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
-import {
-    getEntriesAggregate,
-    getEntriesByDrillLiteral,
-    getEntriesTotal,
-} from '~/models/drill-entry.server'
+import { getEntriesAggregate, getEntriesByDrillLiteral, getEntriesTotal } from '~/models/drill-entry.server'
 import { requireUser } from '~/session.server'
 import { dateFromDaysOptional, dbTimeToString, toDateString } from '~/util'
 
@@ -29,8 +27,11 @@ export async function loader({ request }: LoaderArgs) {
         created_at: toDateString(e.created_at.toString()),
     }))
 
-    const passingData = await getEntriesByDrillLiteral({drillName: "Passing Drill", userId: id, interval})
-    const passingRatio = passingData.map(e => ({ratio: Math.floor(e.value / (e.outOf as number) * 100), created_at: toDateString(e.created_at.toString())}))
+    const passingData = await getEntriesByDrillLiteral({ drillName: 'Passing Drill', userId: id, interval })
+    const passingRatio = passingData.map((e) => ({
+        ratio: Math.floor((e.value / (e.outOf as number)) * 100),
+        created_at: toDateString(e.created_at.toString()),
+    }))
     const speedAggregations = await getEntriesAggregate({ drillName: 'Speed Drill', userId: id, interval })
     const dribblingAggregations = await getEntriesAggregate({ drillName: 'Dribbling Speed', userId: id, interval })
     const squatAggregations = await getEntriesAggregate({ drillName: 'Squat Drill', userId: id, interval })
@@ -47,7 +48,7 @@ export async function loader({ request }: LoaderArgs) {
         passingAggregations,
         jumpingAggregations,
         shootingRatio,
-        passingRatio
+        passingRatio,
     })
 }
 
@@ -64,7 +65,6 @@ export default function Overall() {
         username,
     } = useLoaderData<typeof loader>()
 
-
     const filter = useFetcher<typeof loader>()
 
     const lifetimePie = [
@@ -79,7 +79,59 @@ export default function Overall() {
             fill: orangeAccent,
         },
     ]
-    
+
+    const [downloadReady, setDownloadReady] = useState(false)
+
+    useEffect(() => {
+        setDownloadReady(true)
+    }, [])
+
+    const csvData = [
+        {
+            category: 'Speed',
+            averageTime: dbTimeToString((speedAggregations.average || filter?.data?.speedAggregations.average || null)),
+            bestTime: dbTimeToString(speedAggregations.min || filter?.data?.speedAggregations.min || null),
+        },
+        {
+            category: 'Shooting',
+            shotsMade: filter?.data?.shootingAggregations._sum.value || shootingAggregations._sum.value,
+            shotsAttempted: filter?.data?.shootingAggregations._sum.outOf || shootingAggregations._sum.outOf
+        },
+        {
+            category: 'Dribbling',
+            averageTime: dbTimeToString((dribblingAggregations.average || filter?.data?.dribblingAggregations.average || null)),
+            bestTime: dbTimeToString((dribblingAggregations.min || filter?.data?.dribblingAggregations.min || null))
+        },
+        {
+            category: 'Passing',
+            passesCompleted: filter?.data?.passingAggregations._sum.value || passingAggregations._sum.value,
+            passesAttempted: filter?.data?.passingAggregations._sum.outOf || passingAggregations._sum.outOf
+        },
+        {
+            category: 'Squat',
+            averageTime: dbTimeToString((squatAggregations.average || filter?.data?.squatAggregations.average || null)),
+            bestTime: dbTimeToString(squatAggregations.min || filter?.data?.squatAggregations.min || null),
+        },
+        {
+            category: 'Jumping',
+            averageJumpHeight: (filter?.data?.jumpingAggregations.average || jumpingAggregations.average)?.toFixed(1),
+            bestJumpHeight: (filter.data?.jumpingAggregations.max) || jumpingAggregations.max,
+        }
+
+    ]
+
+    const csvHeaders = [
+        { label: 'Category', key: 'category' },
+        { label: 'Average', key: 'averageTime' },
+        { label: 'Best', key: 'bestTime' },
+        { label: 'Shots Made', key: 'shotsMade'},
+        { label: 'Shots Attempted', key: 'shotsAttempted'},
+        {label: 'Passes Completed', key: 'passesCompleted'},
+        {label: 'Passes Attempted', key: 'passesAttempted'},
+        {label: "Average Jump Height", key: 'averageJumpHeight'},
+        {label: "Best Jump Height", key: 'bestJumpHeight'}
+    ]
+
     return (
         <div>
             <div className="report-card-header">
@@ -93,15 +145,23 @@ export default function Overall() {
                         <button onClick={() => filter.load(`/${username}/stats?index&interval=30`)} className="filter-button month">
                             Month
                         </button>
-                        <button onClick={() => filter.load(`/${username}/stats?index&interval=365`)} className="filter-button year">Year</button>
-                        <button onClick={() => filter.load(`/${username}/stats?index`)}  className="filter-button lifetime">Lifetime</button>
+                        <button onClick={() => filter.load(`/${username}/stats?index&interval=365`)} className="filter-button year">
+                            Year
+                        </button>
+                        <button onClick={() => filter.load(`/${username}/stats?index`)} className="filter-button lifetime">
+                            Lifetime
+                        </button>
                     </div>
                     <div className="export-button-group">
                         <button onClick={() => window.print()} className="print-btn no-print">
                             Prin
                         </button>
 
-                        <button className="export">Exp</button>
+                        {downloadReady && (
+                            <CSVLink headers={csvHeaders} data={csvData}>
+                                Export
+                            </CSVLink>
+                        )}
                     </div>
                 </div>
             </div>
@@ -155,11 +215,11 @@ export default function Overall() {
                     <h4>Strength</h4>
                     <div>
                         <p className="table-stat-name">Avg. Squat Duration w/Weights</p>
-                        <p>{`${dbTimeToString(filter?.data?.squatAggregations.min || squatAggregations.min)}` || 'No data'}</p>
+                        <p>{`${dbTimeToString(filter?.data?.squatAggregations.average || squatAggregations.average)}` || 'No data'}</p>
                     </div>
                     <div>
                         <p className="table-stat-name">Best Squat Duration w/Weights</p>
-                        <p>{`${dbTimeToString(filter?.data?.squatAggregations.average || squatAggregations.average)}` || 'No data'}</p>
+                        <p>{`${dbTimeToString(filter?.data?.squatAggregations.min || squatAggregations.min)}` || 'No data'}</p>
                     </div>
                 </div>
                 <div className="stat-row flex-r accent-2">
@@ -249,7 +309,7 @@ export default function Overall() {
                             <CartesianGrid strokeDasharray="3 3" />
                             <Tooltip />
                             <Legend />
-                            <Line type="monotone" dataKey="ratio" stroke={orangeAccent} strokeWidth={strokeWidth}fillOpacity={1} fill="url(#colorUv2)" />
+                            <Line type="monotone" dataKey="ratio" stroke={orangeAccent} strokeWidth={strokeWidth} fillOpacity={1} fill="url(#colorUv2)" />
                         </LineChart>
                     </ResponsiveContainer>
                 </div>
