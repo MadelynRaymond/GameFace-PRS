37d9b36eacd82ea2e71720ac875040399de854ce
torbryan <4bryan.torres@gmail.com>
2023-02-07 21:07:54 -0500

    Fixed Redundent Code. WIP loading route

--- app/routes/change-password/changepassword.tsx
+++ app/routes/change-password/changepassword.tsx
@@ -1,39 +1,46 @@
-import type { Password, User } from '@prisma/client'
-import { LoaderArgs, json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { z } from "zod";
+import { LoaderArgs, json, redirect } from '@remix-run/node'
+import { Form, useActionData, useTransition } from '@remix-run/react'
+import { z } from 'zod'
+import { changePassword, getUserByEmail, getUserById, verifyLogin } from '~/models/user.server'
+import { requireUser } from '~/session.server'
+import { action } from '../staff/athletes/$athleteId/fetch'
+export const ChangePasswordSchema = z
+    .object({
+        currentPassword: z.string().min(4),
+        newPassword: z.string().min(4),
+        confirmPassword: z.string().min(4),
+    })
+    .refine((data) => data.newPassword === data.confirmPassword, 'Passwords must match')
 
-import { requireUser } from "~/session.server";
-import {changePassword, getUserByEmail, getUserById, verifyLogin } from '~/models/user.server';
-
-
-export const ChangePasswordSchema = z.object({
-    currentPassword: z.string().min(8),
-    newPassword: z.string().min(8),
-    confirmPassword: z.string().min(8)
-  }).refine((data) => data.newPassword === data.confirmPassword, 'Passwords must match')
-  
-
-  export async function loader({ request }: LoaderArgs) {
-    
-    // const { username, id } = await requireUser(request)
-    // const userId = id
-    // const password = 'mynewpassword';
-    // const getPassword = await changePassword({userId, password})
+export async function loader({ request }: LoaderArgs) {
+    const { username, id } = await requireUser(request)
     try {
-        const data = await json(request);
-        const validatedData = await ChangePasswordSchema.parseAsync(data);
-        const { username, id } = await requireUser(request)
-        const user = await getUserById(id);
-        const email = await getUserByEmail(username);
-        if (!user) {
-          throw new Error('User not found')
-        }
-        const isPasswordCorrect = await verifyLogin(user.email, validatedData.currentPassword);
-        if (!isPasswordCorrect) {
-            throw new Error('Incorrect password')
-        }
-      } catch (error) {
+        const formData = await request.formData()
+        const data = await Object.fromEntries(formData)
+        const validatedData = await ChangePasswordSchema.parseAsync(data)
+
+        await changePassword({ userId: id, password: validatedData.newPassword })
+    } catch (error) {
         console.error(error)
     }
-  }
+}
+
+export default function ChangePassword() {
+    const actionData = useActionData<typeof action>()
+    const transition = useTransition()
+    return (
+        <div
+            style={{
+                height: '75vh',
+            }}
+            className="form-center"
+        >
+            <Form method="post">
+                <h1>Change Password</h1>
+                <input type="password" name="password" id="password" placeholder="New Password" />
+                <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password" />
+                <input className="btn" disabled={transition.state === 'loading'} type="submit" value="Change password" />
+            </Form>
+        </div>
+    )
+}
