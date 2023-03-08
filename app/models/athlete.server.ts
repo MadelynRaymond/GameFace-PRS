efd4c5c977937f565305174e2690f88959dc88ec
Matt <n01437978@unf.edu>
2023-03-08 12:07:58 -0500

    Revert "allow email to remain auto-populated"
    This reverts commit 972a83b619d1d23d837fd64896bce1f1a86dedab.

--- app/models/athlete.server.ts
+++ app/models/athlete.server.ts
@@ -81,7 +81,10 @@ export async function updateAthleteProfile(
     const userWithEmail = await prisma.user.findFirst({ where: { email } })
 
     if (userWithEmail?.email === email) {
-        if (userWithEmail?.id !== userId) {
+        if (userWithEmail?.id === userId) {
+            throw new Error(`You must change the email from the current email`)
+        }
+        else {
             throw new Error(`User with email ${email} already exists`)
         }
     }
