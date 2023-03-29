73ee2f37c302d3c39b151c5acf96378e6fbbb1a8
torbryan <4bryan.torres@gmail.com>
2023-03-29 16:45:51 -0400

    Fixed SendEmail html property

--- app/routes/forgot-password/index.tsx
+++ app/routes/forgot-password/index.tsx
@@ -1,7 +1,6 @@
 import type { ActionArgs } from '@remix-run/node'
 import { json, redirect } from '@remix-run/node'
-import { Form, useActionData, useCatch, useTransition } from '@remix-run/react'
-
+import { Form, useActionData, useTransition } from '@remix-run/react'
 import { sendEmail } from '~/mailer'
 import { createTokenForUser } from '~/models/token.server'
 import { getUserByEmail } from '~/models/user.server'
@@ -29,7 +28,20 @@ export async function action({ request }: ActionArgs) {
         )
     }
     const user = await getUserByEmail(email)
+    if (!isProbablyEmail(email)) {
+        return json(
+            {
+                error: 'Please enter a valid email',
+            },
+            { status: 400 }
+        )
+    }
+    const user = await getUserByEmail(email)
 
+    if (!user) {
+        throw new Response('Not Found', { status: 404 })
+    }
+    const token = await createTokenForUser(email)
     if (!user) {
         throw new Response('Not Found', { status: 404 })
     }
@@ -38,23 +50,25 @@ export async function action({ request }: ActionArgs) {
     if (!token) {
         throw new Response('Unexpected Error Occured', { status: 500 })
     }
+    if (!token) {
+        throw new Response('Unexpected Error Occured', { status: 500 })
+    }
 
     const resetLink = `http://${process.env.BASE_URL || 'localhost:3000'}/reset-password/link?id=${user.id}&token=${token.token}`
-
-    
 
     await sendEmail(
         {
             subject: 'Reset Password',
-            body: `
-                <a  href="${resetLink}" 
-                style="background-color:#df7861; border:2px solid black; color:white; padding:1rem 1.5rem; margin:auto; margin-top:6px; display:block; width:183px; font-family:'Montserrat',sans-serif!important; border-radius:12px; text-align:center; text-decoration:none; font-size:1rem;">
+            body: `<p>Please click the button below to reset your password:</p>
+                <a href="${resetLink}" style="background-color:#DF7861;border:2px solid black;color:white;padding:1rem 1.5rem;margin:auto;margin-top:84px;display:block;width:200px;font-family:'Montserrat',sans-serif!important;border-radius:50px;transition-duration:0.1s;transition-timing-function:ease-in;box-shadow:4px 4px 0 black;text-align:center;text-decoration:none;">
                   Reset Password
                 </a>`,
         },
         email
     )
 
+    return redirect(email)
+}
     return redirect(email)
 }
 
@@ -76,27 +90,4 @@ export default function Index() {
             </Form>
         </div>
     )
-}
-//The CatchBoundary component only handles errors that are thrown during rendering
-export function CatchBoundary() {
-    const caught = useCatch()
-
-    if (caught.status === 404) {
-        return (
-            <div className="flex justify-center">
-                <h2>Email-Address-Error: No Account Exists With Email Address</h2>
-            </div>
-        )
-<<<<<<< HEAD
-=======
-    }
-    if (caught.status === 500) {
-        return (
-            <div className="flex justify-center">
-                <h2>Password-Reset-Error: Link Expired</h2>
-            </div>
-        )
-    }
-
-    throw new Error(`Unexpected caught response with status: ${caught.status}`)
 }
