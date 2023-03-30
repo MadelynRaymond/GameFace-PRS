f953f96fba1815a0a0edf8269094291a97f8d8d6
torbryan <4bryan.torres@gmail.com>
2023-03-30 11:07:58 -0400

    Formatting Issue With Existing Reset Password Btn

--- app/email-template.ts
+++ app/email-template.ts
@@ -28,7 +28,7 @@ width="100%">
                         line-height: 27px;
                         padding: 55px 12.5% 40px 12.5%;
                       ">
-                      Hi <span style="color: #333; text-decoration: none;"> ${recipient},</span><<br />
+                      Hi <span style="color: #333; text-decoration: none;"> ${recipient},</span><br/>
                       &nbsp;
                       <h1 style="
                           color: #df7861;
@@ -55,20 +55,6 @@ width="100%">
                         <table border="0" cellpadding="0" cellspacing="0" style="display: inline-block">
                           <tbody>
                             <tr>
-                              <td align="center" bgcolor="#DF7861" height="57" style="border-radius: 20px"
-                                valign="middle" width="219">
-                                <!-- <a href=""
-                                  style="
-                                    font-size: 18px;
-                                    font-family: Arial, Helvetica,
-                                    sans-serif;
-                                    font-weight: bold;
-                                    color: #ffffff;
-                                    text-decoration: none;
-                                  " target="_blank"
-                                  data-saferedirecturl="">Reset
-                                  Password</a>
-                              </td> -->
                               <div>${resetLink}</div>
                             </tr>
                           </tbody>
