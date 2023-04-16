7215a59d734d97f315423cd7c73033a37a27be6f
torbryan <4bryan.torres@gmail.com>
2023-04-16 18:54:23 -0400

    Fixed Error Handling Issue's.
    1. Fixed Error Handling by removing the Status Code Error's and unnecessary formData.get on newEmail and confirmEmail, shouldn't be called since the value will be null.
    
    2. Improved the Error Messages in Form Boxes Displayed within the ChangeEmailSchema. And added missing variable to validate the email.
    
    3.  Added comments to the file for skimming it easier.

--- app/routes/$username/reset-email/$change.tsx
+++ app/routes/$username/reset-email/$change.tsx
@@ -5,18 +5,20 @@ import { Form, Link, useActionData, useCatch, useLoaderData } from '@remix-run/r
 import jwtDecode from 'jwt-decode'
 import jwt from 'jsonwebtoken'
 import { changeEmail } from '~/models/user.server'
-import { string, z } from 'zod'
+import { z } from 'zod'
 import { logout } from '~/session.server'
-import { isProbablyEmail } from '~/util'
 
-const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+//ZOD-ERROR-MSGS: ChangeEmail Form Validation Data
+const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+const INVALID_EMAIL_ERROR = 'Must be a valid email address'
+const EMAILS_DO_NOT_MATCH_ERROR = 'Emails must match'
 
 const ChangeEmailSchema = z
     .object({
-        newEmail: z.string().email().regex(emailRegex, 'Must be a valid email address'),
-        confirmEmail: z.string().email().regex(emailRegex, 'Must be a valid email address'),
+        newEmail: z.string().email().regex(EMAIL_REGEX, INVALID_EMAIL_ERROR),
+        confirmEmail: z.string().email().regex(EMAIL_REGEX, INVALID_EMAIL_ERROR),
     })
-    .refine((data) => data.newEmail === data.confirmEmail, { message: 'Emails must match!' })
+    .refine((data) => data.newEmail === data.confirmEmail, EMAILS_DO_NOT_MATCH_ERROR)
 
 type ChangeEmailErrors = z.inferFlattenedErrors<typeof ChangeEmailSchema>
 type ActionData = {
@@ -24,14 +26,16 @@ type ActionData = {
     updateErrors?: string
 }
 
+// JWT: Verifies the Email & UserId from the URL Generated
 type Token = {
     data?: {
         email: string
         userId: number
     }
-    exp: number
+    exp: number 
 }
 export async function loader({ request }: LoaderArgs) {
+    //URL contains these three query parameters, to ensure that they are present in the URL.
     const url = new URL(request.url)
     const token = url.searchParams.get('token')
     const userId = url.searchParams.get('id')
@@ -41,7 +45,7 @@ export async function loader({ request }: LoaderArgs) {
         throw new Response('Not Found', { status: 404 })
     }
 
-    //Checks if the token is expired
+    // Checks whether the (JWT) is valid or not
     if (!jwt.verify(token, 'SECRET')) {
         throw new Response('Not Authorized', { status: 400 })
     }
@@ -54,40 +58,39 @@ export async function loader({ request }: LoaderArgs) {
     }
 
     throw new Response('Not Authorized', { status: 400 })
-    
 }
 
 export async function action({ request }: ActionArgs) {
     const formData = await request.formData()
+    const data = await Object.fromEntries(formData)
     const userId = formData.get('user-id')
-    const newEmail = formData.get('newEmail')
-    const confirmEmail = formData.get('confirmEmail')
     const exp = formData.get('token')
 
-    if (typeof newEmail !== 'string' || newEmail.length === 0) {
-        return json(
-            {
-                error: 'Email not provided',
-            },
-            { status: 400 }
-        )
-    }
-    if (typeof confirmEmail !== 'string' || confirmEmail.length === 0) {
-        return json(
-            {
-                error: 'Email not provided',
-            },
-            { status: 400 }
-        )
-    }
-    if (!isProbablyEmail(newEmail) || !isProbablyEmail(confirmEmail)) {
-        return json(
-            {
-                error: 'Please enter a valid email',
-            },
-            { status: 400 }
-        )
-    }
+    //Zod Not Showing Up Because of this code, (Last if statement)
+    // if (typeof newEmail !== 'string' || newEmail.length === 0) {
+    //     return json(
+    //         {
+    //             error: 'Email not provided',
+    //         },
+    //         { status: 400 }
+    //     )
+    // }
+    // if (typeof confirmEmail !== 'string' || confirmEmail.length === 0) {
+    //     return json(
+    //         {
+    //             error: 'Email not provided',
+    //         },
+    //         { status: 400 }
+    //     )
+    // }
+    // if (!isProbablyEmail(newEmail) || !isProbablyEmail(confirmEmail)) {
+    //     return json(
+    //         {
+    //             error: 'Please enter a valid email',
+    //         },
+    //         { status: 400 }
+    //     )
+    // }
 
     if (!userId || !exp) {
         throw new Response('Unexpected Error', { status: 500 })
@@ -96,25 +99,21 @@ export async function action({ request }: ActionArgs) {
     if (Date.now() >= parseInt(exp as string) * 1000) {
         throw new Response('Not Authorized', { status: 400 })
     }
-    if (newEmail !== confirmEmail) {
-        return json({ errors: [{ path: ['confirmEmail'], message: 'Emails must match!' }] })
-    }
-
     try {
-        await changeEmail({ userId: parseInt(userId as string), email: newEmail as string })
+        const validatedEmailData = await ChangeEmailSchema.parseAsync(data)
+        await changeEmail({ userId: parseInt(userId as string), email: validatedEmailData.newEmail })
     } catch (error) {
         if (error instanceof z.ZodError) {
             return json({ errors: error.flatten() as ChangeEmailErrors })
         }
     }
-
-    await changeEmail({ userId: parseInt(userId as string), email: newEmail as string })
+    // await changeEmail({ userId: parseInt(userId as string), email: newEmail as string })
     const username = request.url.split('/')[2]
     return redirect(`/${username}/profile`)
     //  return await logout(request) Unsure if we want to logout after successful change or redirect back to profile like changePass
 }
 
-export default function () {
+export default function ChangeEmail() {
     const actionData = useActionData<ActionData>()
     const { userId, token } = useLoaderData<typeof loader>()
 
@@ -131,24 +130,17 @@ export default function () {
                     <label htmlFor="newEmail">New Email:</label>
                     <input type="text" name="newEmail" id="newEmail" placeholder="New Email Address" />
                     {actionData?.errors?.fieldErrors.newEmail && <span className="error-text">{actionData.errors.fieldErrors.newEmail[0]}</span>}
+
                     <label htmlFor="confirmEmail">Confirm Email</label>
                     <input type="text" name="confirmEmail" id="confirmEmail" placeholder="Confirm Email Address" />
                     {actionData?.errors?.fieldErrors.confirmEmail && <span className="error-text">{actionData.errors.fieldErrors.confirmEmail[0]}</span>}
                     <input type="hidden" name="user-id" value={userId} />
                     <input type="hidden" name="token" value={token.exp} />
                 </div>
-                <div className="flex gap-3">
-                    <button className="btn" style={{ display: 'block' }} type="submit">
-                        Change Email{' '}
-                    </button>
-                    <Link style={{ color: 'white' }} className="btn" to={`/`}>
-                        Cancel
-                    </Link>
-                </div>
-                <span style={{ display: 'block' }} className="error-text">
-                    {/* {actionData?.updateErrors}
-                    {actionData?.errors?.formErrors} */}
-                </span>
+                <button className="btn" type="submit">
+                    Change Email
+                </button>
+                {actionData?.errors?.formErrors && <span className="error-text">{actionData.errors.formErrors[0]}</span>}
             </Form>
         </div>
     )
@@ -161,20 +153,18 @@ export function CatchBoundary() {
     if (caught.status === 400) {
         return (
             <div
-            style={{
-                height: '75vh',
-            }}
-            className="form-center"
-        >
-            <Form>
-                <h1>Not Authorized: Unique URL Modified</h1>
-                <p style={{ fontSize: '22px' }}>Email-Address-Update: Not Authorized</p>
-            
-            </Form>
+                style={{
+                    height: '75vh',
+                }}
+                className="form-center"
+            >
+                <Form>
+                    <h1>Not Authorized: Unique URL Modified</h1>
+                    <p style={{ fontSize: '22px' }}>Email-Address-Update: Not Authorized</p>
+                </Form>
             </div>
         )
     }
 
     throw new Error(`Unexpected caught response with status: ${caught.status}`)
 }
-
