e19c91faae1b245794ee20b1a0b51af4b2b52e04
Matt <63518180+mreid21@users.noreply.github.com>
2023-04-28 16:02:27 -0400

    fixed logout button forcing click on text (#70)

--- app/components/NavLinks.tsx
+++ app/components/NavLinks.tsx
@@ -1,15 +1,19 @@
 import { User } from "@prisma/client"
-import { Form, Link } from "@remix-run/react"
+import { Form, Link, useSubmit } from "@remix-run/react"
 
 export default function NavLinks({user}: {user: Omit<User, 'createdAt' | 'updatedAt'> | null}) {
+  const submit = useSubmit()
+
+  const logout = () => {
+    submit(null, { action: '/logout', method: 'post', replace: true })
+  }
+
   if (user) {
     return (
     <ul className="nav-links">
         {user.role === 'STAFF' && <Link  className="orange-background" to={'/staff/athletes'}>Athletes</Link>}
         {user.role !== 'STAFF' && <Link className="purple-background" to={`/${user.username}/stats`}>My Stats</Link>}
-        <Form className="logout-btn orange-background" method="post" action="/logout">
-          <button style={{width: '100%', height: '100%'}} type="submit">Logout</button>
-        </Form>
+        <Link to={"/logout"} className="orange-background" onClick={logout}>Logout</Link>
         {user.role !== 'STAFF' && <Link className="red-background"to={`/${user.username}/profile`}>Profile</Link>}
     </ul>
     )
